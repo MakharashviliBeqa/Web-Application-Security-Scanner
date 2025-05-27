2fd1975f289741f4655f9de58660581e226f33cb
MakharashviliBeqa <201153206+MakharashviliBeqa@users.noreply.github.com>
2025-05-27 15:33:49 +0400

    .

---
+++ test.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib.parse
+import colorama
+import re
+from concurrent.futures import ThreadPoolExecutor
+import sys
+from typing import List, Dict, Set
+
+class WebSecurityScanner:
+    def __init__(self, target_url: str, max_depth: int = 3):
+        self.target_url = target_url
+        self.max_depth = max_depth
+        self.visited_urls: Set[str] = set()
+        self.vulnerabilities: List[Dict] = []
+        self.session = requests.Session()
+        colorama.init()
